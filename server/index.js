0a9a431b23593c08703bd1791dc6f557ba149f74
jbcallv <jbcallv@gmail.com>
2022-06-11 20:05:55 -0400

    project setup complete

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running");
+})
