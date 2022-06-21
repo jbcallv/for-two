3248514efdc9b3295c839056aaed4e166eb2e956
jbcallv <jbcallv@gmail.com>
2022-06-21 19:22:44 -0400

    file structure organization and scrollable site

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import "../styles/App.css";
 import Axios from "axios";
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import { Typography } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
@@ -32,37 +32,35 @@ function App() {
         direction="column">
         <Box
           bgcolor="#FBFAF5"
-          height="98vh"
+          minHeight="100vh"
           width="45vw"
           maxWidth="750px"
           minWidth="360px"
         >
-          <List
-            maxHeight="100%"
-            width="100%"
-            overflow="auto"
-          >
-            {postList.map((post) => {
-              return (
+          {postList.map((post) => {
+            return (
+              <ListItem>
                 <Box>
                   <Typography variant="h4" marginLeft={5} marginTop={2}>{post.title}</Typography>
                   <Typography variant="subtitle1" marginLeft={7}>{post.postedBy}</Typography>
                   <Typography variant="subtitle1" marginLeft={7}>{post.datePosted}</Typography>
                   <Divider variant="middle" width="95%" />
-                  <Typography variant="body1" marginLeft={5} marginTop={2} marginBottom={2}>{post.caption}</Typography>
+                  <Typography variant="body1" marginLeft={5} marginRight={5} marginTop={2} marginBottom={2}>{post.caption}</Typography>
                   <Divider color="#000000" />
                 </Box>
-              );
-            })}
-          </List>
+              </ListItem>
+            );
+          })}
 
           <Grid
             container
             item
             justifyContent="center"
+            marginBottom={2}
             alignItems="center">
             <Button
               variant="contained"
+              href="/CreatePost.js"
               sx={{
                 backgroundColor: "#353935",
                 borderRadius: 3,
